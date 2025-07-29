e1226bda579a174e3f0f068b1783ca393db0182b
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-07-29 14:59:47 +0900

    ⭐ 필터 통일/ 씬전환 코드 추가

--- Eclipse/Eclipse/Moon_Scene.cpp
+++ Eclipse/Eclipse/Moon_Scene.cpp
@@ -95,6 +95,22 @@ void Moon_Scene::Update()
 	{
 		SceneManager::Get().ChangeScene(EclipseApp::ARON);
 	}
+	if (Input::GetKeyDown('4'))
+	{
+		SceneManager::Get().ChangeScene(EclipseApp::WOO);
+	}
+	if (Input::GetKeyDown('5'))
+	{
+		SceneManager::Get().ChangeScene(EclipseApp::MENU);
+	}
+	if (Input::GetKeyDown('6'))
+	{
+		SceneManager::Get().ChangeScene(EclipseApp::PLAY);
+	}
+	if (Input::GetKeyDown('7'))
+	{
+		SceneManager::Get().ChangeScene(EclipseApp::END);
+	}
 
 	// AABB ±×¸®±â 
 	ground_col->DebugColliderDraw();
