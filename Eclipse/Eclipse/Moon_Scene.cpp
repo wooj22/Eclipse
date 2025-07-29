c356fbbf071a2c385296c18a2f55eee6b782b98a
MoonSun <98299225+MoonSun-v@users.noreply.github.com>
2025-07-29 11:53:52 +0900

    Feat | Player, 테스트용 맵 배치

--- Eclipse/Eclipse/Moon_Scene.cpp
+++ Eclipse/Eclipse/Moon_Scene.cpp
@@ -1,22 +1,73 @@
 #include "Moon_Scene.h"
 #include "EclipseApp.h"
 #include "../Direct2D_EngineLib/SceneManager.h"
+#include "../Direct2D_EngineLib/SpriteRenderer.h"
 
 void Moon_Scene::Awake()
 {
-	// camera init
+	// [ camera ]
 	cam = CreateObject<GameObject>();
 	cam->AddComponent<Transform>();
 	cam->AddComponent<Camera>(1920, 1080);
 
-	// create gameobject
-	// title sample
+	// [ title ]
 	title_text = CreateObject<UI_Text>();
 	title_text->rectTransform->SetPosition(0, 500);
 	title_text->rectTransform->SetSize(500, 100);
 	title_text->screenTextRenderer->SetFontSize(50);
 	title_text->screenTextRenderer->SetColor(D2D1::ColorF(D2D1::ColorF::LightPink));
 	title_text->screenTextRenderer->SetText(L"Moon Scene");
+
+	// [ player ]
+	player = CreateObject<Player>();
+
+	// [ ground ]
+	ground = CreateObject<GameObject>();
+	ground->name = "Ground";
+	ground->AddComponent<Transform>()->SetPosition(0.0f, -300.0f);;
+
+	auto ground_sr = ground->AddComponent<SpriteRenderer>();
+	ground_sr->sprite = ResourceManager::Get().CreateSprite(ResourceManager::Get().CreateTexture2D("../Resource/Moon/Ground.png"), "Ground");
+	// ground_sr->layer = -1;
+
+	ground_col = ground->AddComponent<BoxCollider>();
+	ground_col->size = { 1110.0f, 30.0f };
+
+	auto ground_rb = ground->AddComponent<Rigidbody>();
+	ground_rb->useGravity = false;
+	ground_rb->isKinematic = true;
+
+
+	// [ wall_r ]
+	wall_r = CreateObject<GameObject>();
+	wall_r->name = "Wall";
+	wall_r->AddComponent<Transform>()->SetPosition(500.0f, 0.0f);;
+
+	auto wall_r_sr = wall_r->AddComponent<SpriteRenderer>();
+	wall_r_sr->sprite = ResourceManager::Get().CreateSprite(ResourceManager::Get().CreateTexture2D("../Resource/Moon/Wall.png"), "Wall");
+
+	wall_r_col = wall_r->AddComponent<BoxCollider>();
+	wall_r_col->size = { 30.0f, 750.0f };
+
+	auto wall_r_rb = wall_r->AddComponent<Rigidbody>();
+	wall_r_rb->useGravity = false;
+	wall_r_rb->isKinematic = true;
+
+
+	// [ wall_l ]
+	wall_l = CreateObject<GameObject>();
+	wall_l->name = "Wall";
+	wall_l->AddComponent<Transform>()->SetPosition(-500.0f, 0.0f);;
+
+	auto wall_l_sr = wall_l->AddComponent<SpriteRenderer>();
+	wall_l_sr->sprite = ResourceManager::Get().CreateSprite(ResourceManager::Get().CreateTexture2D("../Resource/Moon/Wall.png"), "Wall");
+
+	wall_l_col = wall_l->AddComponent<BoxCollider>();
+	wall_l_col->size = { 30.0f, 750.0f };
+
+	auto wall_l_rb = wall_l->AddComponent<Rigidbody>();
+	wall_l_rb->useGravity = false;
+	wall_l_rb->isKinematic = true;
 }
 
 void Moon_Scene::Start()
