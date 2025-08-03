77bb5af5a3edfac48b2a0efe04f77df107c6fecb
woojeong <77192050+wooj22@users.noreply.github.com>
2025-08-04 00:37:52 +0900

    Feat | AudioClip    // TODO :: 리소스 매니저로 관리

--- Eclipse/Direct2D_EngineLib/AudioClip.cpp
+++ Eclipse/Direct2D_EngineLib/AudioClip.cpp
@@ -1,1 +1,19 @@
 #include "AudioClip.h"
+
+AudioClip::AudioClip(FMOD::System* system, const std::string& filepath)
+{
+    if (system)
+    {
+        FMOD_RESULT result = system->createSound(filepath.c_str(), FMOD_DEFAULT, nullptr, &sound);
+        if (result != FMOD_OK) sound = nullptr;  
+    }
+}
+
+AudioClip::~AudioClip()
+{
+    if (sound)
+    {
+        sound->release();
+        sound = nullptr;
+    }
+}
