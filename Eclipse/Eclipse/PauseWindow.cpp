24b9f6225de40907aff13cbe89128c613fde771f
koplm118 <67418726+koplm118@users.noreply.github.com>
2025-08-08 15:26:22 +0900

    Feat | 일시 정지 창 추가 및 버튼 기능 구현

---
+++ Eclipse/Eclipse/PauseWindow.cpp
@@ -0,0 +1,106 @@
+#include "PauseWindow.h"
+
+void PauseWindow::Awake()
+{
+	rectTransform = AddComponent<RectTransform>();
+	background_Image = SceneManager::Get().GetCurrentScene()->CreateObject<UI_Image>();
+	window_Image = SceneManager::Get().GetCurrentScene()->CreateObject<UI_Image>();
+	close_Button = SceneManager::Get().GetCurrentScene()->CreateObject<UI_Button>();
+	newGame_Button = SceneManager::Get().GetCurrentScene()->CreateObject<UI_Button>();
+	continuGame_Button = SceneManager::Get().GetCurrentScene()->CreateObject<UI_Button>();
+	quitGame_Button = SceneManager::Get().GetCurrentScene()->CreateObject<UI_Button>();
+	underscore_Image = SceneManager::Get().GetCurrentScene()->CreateObject<UI_Image>();
+
+	background_Image->rectTransform->SetParent(rectTransform);
+	window_Image->rectTransform->SetParent(background_Image->rectTransform);
+	close_Button->rectTransform->SetParent(background_Image->rectTransform);
+
+	continuGame_Button->rectTransform->SetParent(background_Image->rectTransform);
+	newGame_Button->rectTransform->SetParent(continuGame_Button->rectTransform);
+	quitGame_Button->rectTransform->SetParent(continuGame_Button->rectTransform);
+	background_Image->imageRenderer->layer = 21;
+}
+
+void PauseWindow::SceneStart()
+{
+	eventButtons = { continuGame_Button,newGame_Button , quitGame_Button };
+	background_Image->rectTransform->SetSize(1248, 702);
+	auto backImagenTexture = ResourceManager::Get().CreateTexture2D("../Resource/mo/OptionsBackGround.png");
+	background_Image->imageRenderer->sprite = ResourceManager::Get().CreateSprite(backImagenTexture, "OptionsBackGround");
+
+
+	window_Image->rectTransform->SetSize(811, 456);
+	window_Image->imageRenderer->sprite = ResourceManager::Get().CreateSprite(backImagenTexture, "OptionsBackGround");
+
+	close_Button->rectTransform->SetPosition(500, 280);
+	close_Button->rectTransform->SetSize(150, 100);
+	auto closeButtonTexture = ResourceManager::Get().CreateTexture2D("../Resource/mo/BackButton.png");
+	close_Button->imageRenderer->sprite = ResourceManager::Get().CreateSprite(closeButtonTexture, "BackButton");
+
+	continuGame_Button->rectTransform->SetPosition(0, -150);
+	continuGame_Button->rectTransform->SetSize(200, 100);
+	continuGame_Button->imageRenderer->SetBaseColor(D2D1::ColorF(D2D1::ColorF::LightPink, 0));
+	
+	newGame_Button->rectTransform->SetPosition(-200, 0);
+	newGame_Button->rectTransform->SetSize(200, 100);
+	newGame_Button->imageRenderer->SetBaseColor(D2D1::ColorF(D2D1::ColorF::LightPink, 0));
+	
+	quitGame_Button->rectTransform->SetPosition(200, 0);
+	quitGame_Button->rectTransform->SetSize(200, 100);
+	quitGame_Button->imageRenderer->SetBaseColor(D2D1::ColorF(D2D1::ColorF::LightPink, 0));
+
+	//TODOMO : 이미지로하면 텍스트는 제거
+	continuGame_Button->screenTextRenderer->SetColor(D2D1::ColorF(D2D1::ColorF::White));
+	continuGame_Button->screenTextRenderer->SetText(L"이어하기");
+	continuGame_Button->screenTextRenderer->SetFontSize(30);
+	newGame_Button->screenTextRenderer->SetColor(D2D1::ColorF(D2D1::ColorF::White));
+	newGame_Button->screenTextRenderer->SetText(L"처음으로");
+	newGame_Button->screenTextRenderer->SetFontSize(30);
+	quitGame_Button->screenTextRenderer->SetColor(D2D1::ColorF(D2D1::ColorF::White));
+	quitGame_Button->screenTextRenderer->SetText(L"게임종료");
+	quitGame_Button->screenTextRenderer->SetFontSize(30);
+
+	// 밑줄 이미지
+	underscore_Image->rectTransform->SetPosition(0, -30); // TODOMO : 버튼 이미지 나오면 위치 수정
+	underscore_Image->rectTransform->SetSize(200, 150);
+	auto underScoreTexture = ResourceManager::Get().CreateTexture2D("../Resource/mo/Underscore.png");
+	underscore_Image->imageRenderer->sprite = ResourceManager::Get().CreateSprite(underScoreTexture, "Underscore");
+	underscore_Image->SetActive(false); // 초기에는 밑줄 이미지 비활성화
+
+	for (UI_Button* btn : eventButtons)
+	{
+		btn->button->onPointEnterListeners.AddListener(
+			this, std::bind(&PauseWindow::OnPointEnterButton, this, btn));
+
+		btn->button->onPointExitListeners.AddListener(
+			this, std::bind(&PauseWindow::OnPointExitButton, this, btn));
+	}
+
+	newGame_Button->button->onClickListeners.AddListener(
+		this, [this]() { SceneManager::Get().ChangeScene(0); });
+
+	//추후 게임 종료 추가
+	/*quitGame_Button->button->onClickListeners.AddListener(
+		this, [this]() { SceneManager::Get().ChangeScene(0); });*/
+
+	SetActive(false);
+
+}
+
+void PauseWindow::Update()
+{
+}
+
+void PauseWindow::OnPointEnterButton(UI_Button* onButton)
+{
+	prevHoverButton = onButton;
+	onButton->imageRenderer->SetAlpha(1);
+	underscore_Image->rectTransform->SetParent(onButton->rectTransform);
+	underscore_Image->SetActive(true);
+}
+
+void PauseWindow::OnPointExitButton(UI_Button* currButton)
+{
+	if (prevHoverButton == currButton)
+		underscore_Image->SetActive(false);
+}
---
+++ Eclipse/Eclipse/PauseWindow.h
@@ -0,0 +1,29 @@
+#pragma once
+#include "../Direct2D_EngineLib/GameObject.h"
+#include "../Direct2D_EngineLib/UI_Image.h"
+#include "../Direct2D_EngineLib/UI_Button.h"
+#include "../Direct2D_EngineLib/SceneManager.h"
+#include "../Direct2D_EngineLib/Scene.h"
+
+class PauseWindow : public GameObject
+{
+public:
+	UI_Image* background_Image;
+	UI_Image* window_Image;
+	UI_Button* close_Button;
+	UI_Button* newGame_Button;
+	UI_Button* continuGame_Button;
+	UI_Button* quitGame_Button;
+
+	UI_Image* underscore_Image;
+	UI_Button* prevHoverButton = nullptr;
+	std::vector<UI_Button*> eventButtons;
+
+public:
+	void Awake() override;
+	void SceneStart() override;   
+	void Update() override;
+	void OnPointEnterButton(UI_Button* parent);
+	void OnPointExitButton(UI_Button* prevButton);
+};
+
