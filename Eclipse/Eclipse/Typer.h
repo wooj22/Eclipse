94f94c6ba659f978beb5adb1ca3ed38a293ea041
Yang Ujeong <77192050+wooj22@users.noreply.github.com>
2025-08-13 19:33:34 +0900

    Feat | Typer 타이핑 연출

--- Eclipse/Eclipse/Typer.h
+++ Eclipse/Eclipse/Typer.h
@@ -1,8 +1,36 @@
 #pragma once
 #include "../Direct2D_EngineLib/Script.h"
 
+using namespace std;
+
+// 타이핑 연출 스크립트
+
+class ScreenTextRenderer;
 class Typer : public Script
 {
+private:
+	ScreenTextRenderer* textRenderer = nullptr;
+	wstring target_text = L"";
+	wstring cur_text = L"";
+	float typingSpeed = 10.0f;
+	float spacingDelay = 0.3f;
+	float timer = 0.0f;
+	bool isTyping;
 
+	void TypingDirecting();
+
+public:
+	// cycle
+	void Update();
+
+	// funcs
+	void StartTyping(wstring text);
+	void EndTyping();
+
+	// setter
+	void SetTextRenderer(ScreenTextRenderer* st) { if(st) textRenderer = st; }
+
+	// getter
+	bool IsTypingEnd() { return !isTyping; }
 };
 
